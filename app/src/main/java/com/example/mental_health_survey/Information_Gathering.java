ca39e308134ebdccbf28d4230b1f218c7755128f
WDH <nexon320@gmail.com>
2020-10-15 02:38:28 +0900

    rr

---
+++ app/src/main/java/com/example/mental_health_survey/Information_Gathering.java
@@ -0,0 +1,14 @@
+package com.example.mental_health_survey;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Information_Gathering extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.information_gathering);
+    }
+}
---
+++ app/src/main/java/com/example/mental_health_survey/Starting_Screen.java
@@ -0,0 +1,32 @@
+package com.example.mental_health_survey;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Starting_Screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.starting_screen);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), Information_Gathering.class);
+                startActivity(intent);
+                finish();
+
+            }
+        },1000);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+}
